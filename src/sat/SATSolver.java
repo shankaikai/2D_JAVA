de8e07b7ad1c63d99828d4189c4db5a09c234303
Shan Kai <48236492+shankaikai@users.noreply.github.com>
2020-10-19 12:14:49 +0800

    Update SATSolver.java
    Added comments

--- src/sat/SATSolver.java
+++ src/sat/SATSolver.java
@@ -43,41 +43,46 @@ public class SATSolver {
      *         or null if no such environment exists.
      */
     private static Environment solve(ImList<Clause> clauses, Environment env) {
-        if (clauses.isEmpty()) {return env;}
-//      no clauses, the formula is trivially satisfiable.
 
+        // no clauses, the formula is trivially satisfiable.
+        if (clauses.isEmpty()) {
+            return env;
+        }
         Clause smallestClause = clauses.first();
 
         for (Clause cl: clauses) {
-            if (cl.isEmpty()) {return null;}
-//      empty clause, the clause is unsatisfiable.
-
+            // Empty clause, the clause is unsatisfiable.
+            if (cl.isEmpty()) {
+                return null;
+            }
+            
+            // Replace the smallestClause variable with the next clause if it has less literals
             if (cl.size() < smallestClause.size()) {
                 smallestClause = cl;
             }
         }
 
         Literal literal = smallestClause.chooseLiteral();
-//      The variable literal is the first Literal in smallestClause.
+        // The variable literal is the first Literal in smallestClause.
         if (smallestClause.isUnit()) {
             if (literal instanceof PosLiteral) {
                 env = env.putTrue(literal.getVariable());
-//      Positive literal will have bool.TRUE
+            // Positive literal will have bool.TRUE
             } else if (literal instanceof NegLiteral){
                 env = env.putFalse(literal.getVariable());
-//      Negative literal will have bool.FALSE
+                // Negative literal will have bool.FALSE
             }
 
             return solve(substitute(clauses, literal), env);
 
         } else {
             env = env.putTrue(literal.getVariable());
-//      set literal to TRUE
+            // set literal to TRUE
             Environment testLiteral = solve(substitute(clauses, literal),env);
-//      substitute it in all clause
+            // substitute it in all clause
             if (testLiteral == null){
                 env = env.putFalse(literal.getVariable());
-//      set literal to FALSE as testLiteral == null
+            // set literal to FALSE as testLiteral == null
                 return solve(substitute(clauses,literal.getNegation()),env);
             }
             return testLiteral;
@@ -94,13 +99,21 @@ public class SATSolver {
      *            , a literal to set to true
      * @return a new list of clauses resulting from setting l to true
      */
-    private static ImList<Clause> substitute(ImList<Clause> clauses,
-            Literal l) {
+    private static ImList<Clause> substitute(ImList<Clause> clauses, Literal l) {
+
+        // Initiate new clause list
         ImList<Clause> new_list = new EmptyImList<Clause>();
+
+        // Iterate through all the clauses in the clause list
         for (Clause clause : clauses) {
+
+            // Check if clause contains the literal l or its negation
             if (clause.contains(l) || clause.contains(l.getNegation())) {
+                // Set literal to true
                 clause.reduce(l);
             }
+
+            // If the result is not null, add the clause to the new clause list
             if (clause != null) {
                 new_list.add(clause);
             }
